7621675074aeb2cdfd2cf782aada87bc566124ba
artemkoorochka <artem@koorochka.com>
2019-12-18 14:39:25 +0200

    Highload sample

--- public_html/system/index.php
+++ public_html/system/index.php
@@ -1,71 +1,28 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+
+
+//CIBlockElement::SetPropertyValues(2615, 2, "KIDIAN", "FACTORY");
+
+
+CModule::IncludeModule("highloadblock");
+
+use Bitrix\Highloadblock;
+use Bitrix\Main\Entity;
+$hlblock = Highloadblock\HighloadBlockTable::getById(6)->fetch();
+$hlEntity = Highloadblock\HighloadBlockTable::compileEntity($hlblock);
+$entDataClass = $hlEntity->getDataClass();
+$sTableID = 'tbl_'.$hlblock['TABLE_NAME'];
+$rsData = $entDataClass::getList(array(
+    "select" => array('*'), //выбираем все поля
+    "filter" => array("UF_NAME" => "KIDIAN"),
+    "order" => array("UF_NAME"=>"ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
+));
+$rsData = new CDBResult($rsData, $sTableID);
+if($arRes = $rsData->Fetch()){
+    d($arRes);
+}
+
 ?>
-
-    <div class="donate">
-        <div class="container">
-            <p>Help keep this project alive</p>
-
-            <ul>
-                <li><a href="#" class="link-small paypal-amount" data-amount="5">$5</a></li>
-                <li><a href="#" class="link-small paypal-amount" data-amount="10">$10</a></li>
-                <li><a href="#" class="link-small paypal-amount" data-amount="20">$20</a></li>
-                <li><a href="#" class="link-small paypal-amount" data-amount="50">$50</a></li>
-                <li><a href="#" target="_blank" class="link-small paypal-donate">Donate</a></li>
-            </ul>
-
-            <i class="hidden">Close</i>
-        </div>
-    </div>
-
-<script>
-
-    var donationBar   = document.getElementsByClassName('donate')[0],
-        donationClose = donationBar.getElementsByTagName('i')[0];
-
-    donationClose.addEventListener('click', function() {
-        donationBar.classList.add('hidden');
-
-        setTimeout(function(){
-            donationBar.classList.add('top');
-            donationBar.classList.remove('hidden');
-        }, 200);
-    });
-
-
-    $(window).scroll(function(){
-        var position = $(this).scrollTop();
-
-        if ( $(window).scrollTop() > 300 ) {
-            donationClose.classList.remove('hidden');
-        } else {
-            donationClose.classList.add('hidden');
-        }
-
-        if ( $(window).scrollTop() > 50 ) {
-            donationBar.classList.add('scrolled');
-        } else {
-            donationBar.classList.remove('scrolled');
-        }
-
-        if ( $(window).scrollTop() == 0 ) {
-            donationBar.classList.remove('top');
-        }
-    });
-
-    var selectedClass = 'paypal-amount-selected',
-        paypalMeUrl = 'https://paypal.me/igorcescobar/';
-
-    $('.paypal-amount').on('click', function(e) {
-        $('.donate a').removeClass(selectedClass).removeClass('active');
-        $(this).addClass(selectedClass).addClass('active');
-        e.preventDefault();
-    });
-
-    $('.paypal-donate').on('click', function() {
-        var amount = $('.' + selectedClass).data('amount') || '';
-        $(this).attr('href',  paypalMeUrl + amount);
-    });
-</script>
 
 <?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
