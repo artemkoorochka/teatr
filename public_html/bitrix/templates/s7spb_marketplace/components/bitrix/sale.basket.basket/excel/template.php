43634ec0d4d7c5043a964432b35a70457460fcbf
artemkoorochka <artem@koorochka.com>
2020-01-29 14:07:03 +0200

    Basic sample

--- public_html/bitrix/templates/s7spb_marketplace/components/bitrix/sale.basket.basket/excel/template.php
+++ public_html/bitrix/templates/s7spb_marketplace/components/bitrix/sale.basket.basket/excel/template.php
@@ -9,64 +9,17 @@
  * @var CBitrixComponentTemplate $this
  * @var array $giftParameters
  */
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-use Bitrix\Main\Localization\Loc;
-Loc::loadLanguageFile(__FILE__);
-
-
-////////////////////////////////////
-///
-//1 Часть: запись в файл
-
-//Создание объекта класса библиотеки
+// Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
 
-//Указываем страницу, с которой работаем
-$objPHPExcel->setActiveSheetIndex(0);
+$string = "Vasja.xlsx";
+//$string = mb_convert_encoding($string, "UTF-8", "Windows-1251");
 
-//Получаем страницу, с которой будем работать
-$active_sheet = $objPHPExcel->getActiveSheet();
+$objPHPExcel->getActiveSheet()
+    //    ->setCellValue('A1', 'Hello')
+    ->setCellValueExplicit("B1", $string, PHPExcel_Cell_DataType::TYPE_STRING);
 
-//Создание новой страницы(пример)
-//$objPHPExcel->createSheet();
-
-//Ориентация и размер страницы
-// $active_sheet->getPageSetup()
-// ->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_PORTRAIT);
-$active_sheet->getPageSetup()
-    ->setOrientation(PHPExcel_Worksheet_PageSetup::ORIENTATION_LANDSCAPE);
-$active_sheet->getPageSetup()
-    ->setPaperSize(PHPExcel_Worksheet_PageSetup::PAPERSIZE_A4);
-
-//Имя страницы
-$active_sheet->setTitle("Данные из docs");
-
-//Ширина стобцов
-$active_sheet->getColumnDimension('A')->setWidth(8);
-$active_sheet->getColumnDimension('B')->setWidth(10);
-$active_sheet->getColumnDimension('C')->setWidth(90);
-
-//Объединение ячеек
-$active_sheet->mergeCells('A1:C1');
-
-//Высота строки
-$active_sheet->getRowDimension('1')->setRowHeight(30);
-
-//Вставить данные(примеры)
-//Нумерация строк начинается с 1, координаты A1 - 0,1
-$active_sheet->setCellValueByColumnAndRow(0, 1, 'Сегодня '.date('d-m-Y'));
-$active_sheet->setCellValue('A3', 'id');
-$active_sheet->setCellValue('B3', 'name');
-$active_sheet->setCellValue('C3', 'info');
-
-//Отправляем заголовки с типом контекста и именем файла
-header("Content-Type:application/vnd.ms-excel");
-header("Content-Disposition:attachment;filename=simple.xlsx");
-
-//Сохраняем файл с помощью PHPExcel_IOFactory и указываем тип Excel
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel15');
-
-//Отправляем файл
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 $objWriter->save('php://output');
 
 ?>
