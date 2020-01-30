19c899a725bdda26e8080417bb6b6834ca4f83ca
artemkoorochka <artem@koorochka.com>
2020-01-30 12:23:26 +0200

    CSaleBasket::GetList sample

--- public_html/system/index.php
+++ public_html/system/index.php
@@ -4,12 +4,39 @@
  * @var CMain $APPLICATION
  */
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->RestartBuffer();
 
-if (CModule::IncludeModule('currency')) {
 
-        $val = 100; // сумма в USD
-        $newval = CCurrencyRates::ConvertCurrency($val, "USD", "RUB");
-        d($val." USD = ".$newval." EUR");
+$arBasketItems = array();
 
+$dbBasketItems = CSaleBasket::GetList(
+    array(
+        "NAME" => "ASC",
+        "ID" => "ASC"
+    ),
+    array(
+        "FUSER_ID" => CSaleBasket::GetBasketUserID(),
+        "LID" => SITE_ID,
+        "ORDER_ID" => "NULL"
+    ),
+    false,
+    false,
+    array("ID", "CALLBACK_FUNC", "MODULE",
+        "PRODUCT_ID", "QUANTITY", "DELAY",
+        "CAN_BUY", "PRICE", "WEIGHT")
+);
+while ($arItems = $dbBasketItems->Fetch())
+{
+    if (strlen($arItems["CALLBACK_FUNC"]) > 0)
+    {
+        CSaleBasket::UpdatePrice($arItems["ID"],
+            $arItems["CALLBACK_FUNC"],
+            $arItems["MODULE"],
+            $arItems["PRODUCT_ID"],
+            $arItems["QUANTITY"]);
+        $arItems = CSaleBasket::GetByID($arItems["ID"]);
+    }
+
+    $arBasketItems[] = $arItems;
 }
+
+d($arBasketItems);
