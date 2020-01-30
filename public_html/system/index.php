bbfeb11bafc83f1c67ed84cbbb136b80535c1981
artemkoorochka <artem@koorochka.com>
2020-01-30 11:48:56 +0200

    Sample ConvertCurrency

--- public_html/system/index.php
+++ public_html/system/index.php
@@ -1,28 +1,15 @@
 <?
+/**
+ * https://dev.1c-bitrix.ru/api_help/currency/developer/ccurrencyrates/ccurrencyrates__convertcurrency.930a5544.php
+ * @var CMain $APPLICATION
+ */
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->RestartBuffer();
 
+if (CModule::IncludeModule('currency')) {
 
-//CIBlockElement::SetPropertyValues(2615, 2, "KIDIAN", "FACTORY");
+        $val = 100; // сумма в USD
+        $newval = CCurrencyRates::ConvertCurrency($val, "USD", "RUB");
+        d($val." USD = ".$newval." EUR");
 
-
-CModule::IncludeModule("highloadblock");
-
-use Bitrix\Highloadblock;
-use Bitrix\Main\Entity;
-$hlblock = Highloadblock\HighloadBlockTable::getById(6)->fetch();
-$hlEntity = Highloadblock\HighloadBlockTable::compileEntity($hlblock);
-$entDataClass = $hlEntity->getDataClass();
-$sTableID = 'tbl_'.$hlblock['TABLE_NAME'];
-$rsData = $entDataClass::getList(array(
-    "select" => array('*'), //выбираем все поля
-    "filter" => array("UF_NAME" => "KIDIAN"),
-    "order" => array("UF_NAME"=>"ASC") // сортировка по полю UF_SORT, будет работать только, если вы завели такое поле в hl'блоке
-));
-$rsData = new CDBResult($rsData, $sTableID);
-if($arRes = $rsData->Fetch()){
-    d($arRes);
 }
-
-?>
-
-<?require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
