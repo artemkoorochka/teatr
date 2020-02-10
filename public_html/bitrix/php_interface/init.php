84f69987f02ce6365fb00f4a2923363844254b7d
artemkoorochka <artem@koorochka.com>
2020-02-10 16:16:42 +0200

    Recalculate basket items before & AFTER order add

--- public_html/bitrix/php_interface/init.php
+++ public_html/bitrix/php_interface/init.php
@@ -11,6 +11,7 @@ $eventManager = EventManager::getInstance();
  */
 $eventManager->AddEventHandler("sale", "OnSaleComponentOrderOneStepOrderProps", "OnSaleComponentOrderOneStepOrderPropsHandler");
 $eventManager->AddEventHandler("sale", "OnBeforeOrderAdd", "OnBeforeOrderAddHandler");
+$eventManager->AddEventHandler("sale", "OnOrderAdd", "OnOrderAddHandler");
 
 /**
  * Iblock element events
@@ -21,9 +22,14 @@ $eventManager->AddEventHandler("iblock", "OnAfterIBlockElementUpdate", array("la
 /**
  * catalog events
  */
+
+/*
 if(CSite::InDir(SITE_DIR . "basket/") || CSite::InDir(SITE_DIR . "order/")){
-    $eventManager->AddEventHandler("catalog", "OnGetOptimalPrice", array("lansyPriceGenerator", "OnGetOptimalPriceHandler"));
+
 }
+*/
+
+$eventManager->AddEventHandler("catalog", "OnGetOptimalPrice", array("lansyPriceGenerator", "OnGetOptimalPriceHandler"));
 
 /**
  * callbacks
@@ -125,39 +131,92 @@ function OnBeforeOrderAddHandler(&$arFields){
             $countPropsFill++;
         }
     }
+    $arFields["USER_DESCRIPTION"] = "";
     if($countProps > $countPropsFill){
-        $arFields["USER_DESCRIPTION"] = "Покупатель запросил обратный звонок. Номер " . $arFields["ORDER_PROP"][22];
+        $arFields["USER_DESCRIPTION"] .= "Покупатель запросил обратный звонок. Номер " . $arFields["ORDER_PROP"][22];
+        $arFields["USER_DESCRIPTION"] .= "\r\n";
     }
 
     ///
     /// BASKET_ITEMS
     /// recalculate
     ///
-    if($_SESSION["SALE_USER_CURRENCY"] != "RUB"){
-        $_SESSION["SALE_USER_CURRENCY"] = "RUB";
-    }
+    // $_SESSION["SALE_USER_CURRENCY"] = "RUB";
     $price = intval($_SESSION["USER_CATALOG_GROUP"]);
     if($price > 0){
+        if($price == 6){
+            $price = 7;
+        }
         $arFields["PRICE"] = 0;
         $arFields["CURRENCY"] = "RUB";
         $neo = new CSaleBasket();
         foreach ($arFields["BASKET_ITEMS"] as $key=>$arItem){
             $arOptPrices = CCatalogProduct::GetByIDEx($arItem["PRODUCT_ID"]);
             $arItem["PRICE"] = $arOptPrices['PRICES'][$price]['PRICE'];
+            $arFields["BASKET_ITEMS"][$key]["PRICE_TYPE_ID"] = $price;
             $arFields["BASKET_ITEMS"][$key]["PRICE"] = $arItem["PRICE"];
+            $arFields["BASKET_ITEMS"][$key]["BASE_PRICE"] = $arItem["BASE_PRICE"];
+            $arFields["BASKET_ITEMS"][$key]["PRODUCT_PRICE_ID"] = null;
+            $arFields["BASKET_ITEMS"][$key]["CURRENCY"] = $arFields["CURRENCY"];
 
             $neo->Update($arItem["ID"], array(
+                "PRODUCT_PRICE_ID" => null,
                 "PRICE" => $arItem["PRICE"],
-                "CURRENCY" => "RUB"
+                "BASE_PRICE" => $arItem["BASE_PRICE"],
+                "CUSTOM_PRICE" => "Y",
+                "PRICE_TYPE_ID" => $price,
+                "CURRENCY" => $arFields["CURRENCY"]
             ));
 
             $arFields["PRICE"] += $arItem["PRICE"] * $arItem["QUANTITY"];
         }
 
+        $catalogGroup = GetCatalogGroup($price);
+        $catalogGroup = $catalogGroup["NAME"];
+        $arFields["USER_DESCRIPTION"] .= "Покупатель выбрал цену для заказа: " . $catalogGroup;
     }
     ///
     ///
+}
 
+function OnOrderAddHandler($ID, $arFields)
+{
+
+    CModule::IncludeModule("sale");
+
+    $price = intval($_SESSION["USER_CATALOG_GROUP"]);
+    if($price > 0) {
+        if ($price == 6) {
+            $price = 7;
+            $arFields["CURRENCY"] = "RUB";
+        }
+
+        AddMessage2Log($price, "OnOrderAddHandler");
+
+        $neo = new CSaleBasket();
+        foreach ($arFields["BASKET_ITEMS"] as $key=>$arItem){
+            $arOptPrices = CCatalogProduct::GetByIDEx($arItem["PRODUCT_ID"]);
+            $arItem["PRICE"] = $arOptPrices['PRICES'][$price]['PRICE'];
+            $arFields["BASKET_ITEMS"][$key]["PRICE_TYPE_ID"] = $price;
+            $arFields["BASKET_ITEMS"][$key]["PRICE"] = $arItem["PRICE"];
+            $arFields["BASKET_ITEMS"][$key]["BASE_PRICE"] = $arItem["BASE_PRICE"];
+            $arFields["BASKET_ITEMS"][$key]["CUSTOM_PRICE"] = "Y";
+            $arFields["BASKET_ITEMS"][$key]["CURRENCY"] = $arFields["CURRENCY"];
+
+            $neo->Update($arItem["ID"], array(
+                "PRODUCT_PRICE_ID" => null,
+                "PRICE" => $arItem["PRICE"],
+                "CUSTOM_PRICE" => "Y",
+                "BASE_PRICE" => $arItem["BASE_PRICE"],
+                "PRICE_TYPE_ID" => $price,
+                "CURRENCY" => $arFields["CURRENCY"]
+            ));
+        }
+
+        AddMessage2Log($arFields, "OnOrderAddHandler");
+
+
+    }
 }
 
 // </editor-fold>
