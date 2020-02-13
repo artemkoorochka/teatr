15a4fe408e3ea0a802859fb3885ef050721be5c3
artemkoorochka <artem@koorochka.com>
2020-02-13 09:54:01 +0200

    Include areas iblock

---
+++ public_html/bitrix/templates/s7spb_marketplace/footer.php
@@ -0,0 +1,371 @@
+<?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)die();
+use Bitrix\Main\Localization\Loc;
+Loc::loadMessages(__FILE__);
+?>
+					</div>
+				</section>
+			</main>
+		</div>
+		<footer class="s7sbp--marketplace--footer">
+			<div class="s7sbp--marketplace--wrapper-inner">
+				<div class="s7sbp--marketplace--footer--left">
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:news.detail",
+                        "include.area",
+                        array(
+                            "COMPONENT_TEMPLATE" => "include.area",
+                            "IBLOCK_TYPE" => "marketplace",
+                            "IBLOCK_ID" => "10",
+                            "ELEMENT_ID" => "38856",
+                            "ELEMENT_CODE" => "",
+                            "CHECK_DATES" => "Y",
+                            "FIELD_CODE" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "PROPERTY_CODE" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "IBLOCK_URL" => "",
+                            "DETAIL_URL" => "",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "N",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "CACHE_TYPE" => "A",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_GROUPS" => "N",
+                            "SET_TITLE" => "N",
+                            "SET_CANONICAL_URL" => "N",
+                            "SET_BROWSER_TITLE" => "N",
+                            "BROWSER_TITLE" => "-",
+                            "SET_META_KEYWORDS" => "N",
+                            "META_KEYWORDS" => "-",
+                            "SET_META_DESCRIPTION" => "N",
+                            "META_DESCRIPTION" => "-",
+                            "SET_LAST_MODIFIED" => "N",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "ADD_ELEMENT_CHAIN" => "N",
+                            "ACTIVE_DATE_FORMAT" => "",
+                            "USE_PERMISSIONS" => "N",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "DISPLAY_DATE" => "N",
+                            "DISPLAY_NAME" => "N",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "USE_SHARE" => "N",
+                            "PAGER_TEMPLATE" => ".default",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "PAGER_TITLE" => "Страница",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "SET_STATUS_404" => "N",
+                            "SHOW_404" => "N",
+                            "MESSAGE_404" => ""
+                        ),
+                        false
+                    );?>
+				</div>
+				<div class="s7sbp--marketplace--footer--right">				
+					<div class="s7sbp--marketplace--footer--menu--col">
+
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.detail",
+                            "include.area",
+                            array(
+                                "COMPONENT_TEMPLATE" => "include.area",
+                                "IBLOCK_TYPE" => "marketplace",
+                                "IBLOCK_ID" => "10",
+                                "ELEMENT_ID" => "38858",
+                                "ELEMENT_CODE" => "",
+                                "CHECK_DATES" => "Y",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "IBLOCK_URL" => "",
+                                "DETAIL_URL" => "",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "N",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_GROUPS" => "N",
+                                "SET_TITLE" => "N",
+                                "SET_CANONICAL_URL" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "BROWSER_TITLE" => "-",
+                                "SET_META_KEYWORDS" => "N",
+                                "META_KEYWORDS" => "-",
+                                "SET_META_DESCRIPTION" => "N",
+                                "META_DESCRIPTION" => "-",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "ADD_ELEMENT_CHAIN" => "N",
+                                "ACTIVE_DATE_FORMAT" => "",
+                                "USE_PERMISSIONS" => "N",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "DISPLAY_DATE" => "N",
+                                "DISPLAY_NAME" => "N",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "USE_SHARE" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "N",
+                                "PAGER_TITLE" => "Страница",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => ""
+                            ),
+                            false
+                        );?>
+						<?$APPLICATION->IncludeComponent("bitrix:menu", "bottom", array(
+							"ROOT_MENU_TYPE" => "bottom_company",
+							"MENU_CACHE_TYPE" => "Y",
+							"MENU_CACHE_TIME" => "3600000",
+							"MENU_CACHE_USE_GROUPS" => "N",
+							"MENU_CACHE_GET_VARS" => array(),
+							"MAX_LEVEL" => "1",
+							"USE_EXT" => "N",
+							"DELAY" => "N",
+							"ALLOW_MULTI_SELECT" => "N"
+							),false
+						);?>
+					</div>
+					<div class="s7sbp--marketplace--footer--menu--col">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.detail",
+                            "include.area",
+                            array(
+                                "COMPONENT_TEMPLATE" => "include.area",
+                                "IBLOCK_TYPE" => "marketplace",
+                                "IBLOCK_ID" => "10",
+                                "ELEMENT_ID" => "38859",
+                                "ELEMENT_CODE" => "",
+                                "CHECK_DATES" => "Y",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "IBLOCK_URL" => "",
+                                "DETAIL_URL" => "",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "N",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_GROUPS" => "N",
+                                "SET_TITLE" => "N",
+                                "SET_CANONICAL_URL" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "BROWSER_TITLE" => "-",
+                                "SET_META_KEYWORDS" => "N",
+                                "META_KEYWORDS" => "-",
+                                "SET_META_DESCRIPTION" => "N",
+                                "META_DESCRIPTION" => "-",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "ADD_ELEMENT_CHAIN" => "N",
+                                "ACTIVE_DATE_FORMAT" => "",
+                                "USE_PERMISSIONS" => "N",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "DISPLAY_DATE" => "N",
+                                "DISPLAY_NAME" => "N",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "USE_SHARE" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "N",
+                                "PAGER_TITLE" => "Страница",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => ""
+                            ),
+                            false
+                        );?>
+						<?$APPLICATION->IncludeComponent("bitrix:menu", "bottom", array(
+							"ROOT_MENU_TYPE" => "bottom_services",
+							"MENU_CACHE_TYPE" => "Y",
+							"MENU_CACHE_TIME" => "3600000",
+							"MENU_CACHE_USE_GROUPS" => "N",
+							"MENU_CACHE_GET_VARS" => array(),
+							"MAX_LEVEL" => "1",
+							"USE_EXT" => "N",
+							"DELAY" => "N",
+							"ALLOW_MULTI_SELECT" => "N"
+							),false
+						);?>
+					</div>
+					<div class="s7sbp--marketplace--footer--menu--col">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.detail",
+                            "include.area",
+                            array(
+                                "COMPONENT_TEMPLATE" => "include.area",
+                                "IBLOCK_TYPE" => "marketplace",
+                                "IBLOCK_ID" => "10",
+                                "ELEMENT_ID" => "38860",
+                                "ELEMENT_CODE" => "",
+                                "CHECK_DATES" => "Y",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "IBLOCK_URL" => "",
+                                "DETAIL_URL" => "",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "N",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_GROUPS" => "N",
+                                "SET_TITLE" => "N",
+                                "SET_CANONICAL_URL" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "BROWSER_TITLE" => "-",
+                                "SET_META_KEYWORDS" => "N",
+                                "META_KEYWORDS" => "-",
+                                "SET_META_DESCRIPTION" => "N",
+                                "META_DESCRIPTION" => "-",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "ADD_ELEMENT_CHAIN" => "N",
+                                "ACTIVE_DATE_FORMAT" => "",
+                                "USE_PERMISSIONS" => "N",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "DISPLAY_DATE" => "N",
+                                "DISPLAY_NAME" => "N",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "USE_SHARE" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "N",
+                                "PAGER_TITLE" => "Страница",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => ""
+                            ),
+                            false
+                        );?>
+						<?$APPLICATION->IncludeComponent("bitrix:menu", "bottom", array(
+							"ROOT_MENU_TYPE" => "bottom_partners",
+							"MENU_CACHE_TYPE" => "Y",
+							"MENU_CACHE_TIME" => "3600000",
+							"MENU_CACHE_USE_GROUPS" => "N",
+							"MENU_CACHE_GET_VARS" => array(),
+							"MAX_LEVEL" => "1",
+							"USE_EXT" => "N",
+							"DELAY" => "N",
+							"ALLOW_MULTI_SELECT" => "N"
+							),false
+						);?>
+                        <a href="<?=SITE_DIR?>partner/" class="btn h4 mt-3 d-block"><?=Loc::getMessage("T_S7SPB_MARKETPLACE_FOOTER_PARTNER")?></a>
+					</div>			
+					<div class="s7sbp--marketplace--footer--sochial s7sbp--marketplace--footer--menu--col">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news.detail",
+                            "include.area",
+                            array(
+                                "COMPONENT_TEMPLATE" => "include.area",
+                                "IBLOCK_TYPE" => "marketplace",
+                                "IBLOCK_ID" => "10",
+                                "ELEMENT_ID" => "38857",
+                                "ELEMENT_CODE" => "",
+                                "CHECK_DATES" => "Y",
+                                "FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "IBLOCK_URL" => "",
+                                "DETAIL_URL" => "",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "N",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_GROUPS" => "N",
+                                "SET_TITLE" => "N",
+                                "SET_CANONICAL_URL" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "BROWSER_TITLE" => "-",
+                                "SET_META_KEYWORDS" => "N",
+                                "META_KEYWORDS" => "-",
+                                "SET_META_DESCRIPTION" => "N",
+                                "META_DESCRIPTION" => "-",
+                                "SET_LAST_MODIFIED" => "N",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "ADD_ELEMENT_CHAIN" => "N",
+                                "ACTIVE_DATE_FORMAT" => "",
+                                "USE_PERMISSIONS" => "N",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "DISPLAY_DATE" => "N",
+                                "DISPLAY_NAME" => "N",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "USE_SHARE" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "N",
+                                "PAGER_TITLE" => "Страница",
+                                "PAGER_SHOW_ALL" => "N",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SHOW_404" => "N",
+                                "MESSAGE_404" => ""
+                            ),
+                            false
+                        );?>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+<div id="scroll-to-top" title="<?=Loc::getMessage("T_S7SPB_MARKETPLACE_SCROLL_TOP")?>"><div></div></div>
+<a href="<?=SITE_DIR?>basket/" id="stiky-bascet">
+    <span class="icon-basket"></span>
+    <span class="s7sbp--marketplace--full--basket--counter"></span>
+</a>
+</body>
+</html>
