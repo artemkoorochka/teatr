387112ced8ffa56fbde7bf8414181167c41db6b7
artemkoorochka <artem@koorochka.com>
2020-02-19 12:21:41 +0200

    Конструктор готов. Проектирование методов класса. Их будет 2 основных, задать старт и выполнение загрузки. Планирую переписать весь асинхронный js на jquery и наверное что использовать json вместо eval

--- public_html/bitrix/components/studio7sbp/seller.personal.import.transfer/templates/.default/script.js
+++ public_html/bitrix/components/studio7sbp/seller.personal.import.transfer/templates/.default/script.js
@@ -4,19 +4,50 @@
  * Это не просто синтаксический сахар
  * @param arParams
  */
-// 1. Создаём функцию constructor
+
+/**
+ * Create constructor function
+ * @param arParams
+ */
 function lancyImport(arParams) {
     this.arParams = arParams;
+    this.bWorkFinished = false;
+    this.bSubmit = null;
 }
-// каждый прототип функции имеет свойство constructor по умолчанию,
-// поэтому нам нет необходимости его создавать
 
-// 2. Добавляем метод в прототип
-lancyImport.prototype.sayHi = function() {
+/**
+ * Prototipe methods
+ * каждый прототип функции имеет свойство constructor по умолчанию,
+ * поэтому нам нет необходимости его создавать
+ */
+lancyImport.prototype.showStatus = function() {
     console.info(this.arParams);
+    console.info(this.bWorkFinished);
+    console.info(this.bSubmit);
 };
 
-var lancyImport = new lancyImport({name: "Artem", age: 38});
+lancyImport.prototype.setStart = function (val) {
+    console.info(val);
+};
+
+lancyImport.prototype.workOnload = function (result) {
+    console.info(result);
+};
 
 
-lancyImport.sayHi();
+
+
+
+/**
+ * Using
+ * @type {lancyImport}
+ */
+var lancyImport = new lancyImport({
+    arParams: {param1: 1, param2: 2}
+});
+
+
+lancyImport.showStatus();
+
+lancyImport.setStart("set start");
+lancyImport.workOnload("result to eval");
