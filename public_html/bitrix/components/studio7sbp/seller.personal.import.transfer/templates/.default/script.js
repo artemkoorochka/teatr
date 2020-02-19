c26c09e8661b9361c9c71a6cc5453e2cd5a4469c
artemkoorochka <artem@koorochka.com>
2020-02-19 16:49:54 +0200

    Готовый фрон-энд

--- public_html/bitrix/components/studio7sbp/seller.personal.import.transfer/templates/.default/script.js
+++ public_html/bitrix/components/studio7sbp/seller.personal.import.transfer/templates/.default/script.js
@@ -27,27 +27,90 @@ lancyImport.prototype.showStatus = function() {
 };
 
 lancyImport.prototype.setStart = function (val) {
-    console.info(val);
+
+    var status = "<strong>" + this.arParams.messages.status + ":</strong> " + this.arParams.messages.work;
+    $(this.arParams.status).html(status);
+    if(val){
+        // Preparato set status bar and set progress to null
+        this.showWaitWindow();
+        $(this.arParams.bar)
+            .removeClass("d-none")
+            .find(".progress-bar")
+            .css({width:"0%"})
+            .text("0%")
+            .addClass("progress-bar-animated")
+            .addClass("progress-bar-striped");
+
+        // Ajax
+
+        BX.ajax.runComponentAction(
+            'studio7sbp:seller.personal.import.transfer',
+            'getElements',
+            {
+                mode:'class',
+                data: {
+                    work_start: "Y",
+                    sessid: BX.bitrix_sessid()
+                }
+            }).then(BX.delegate(this.workOnload, this));
+
+    }
+    else{
+        this.closeWaitWindow();
+    }
+
 };
 
-lancyImport.prototype.workOnload = function (result) {
-    console.info(result);
+lancyImport.prototype.workOnload = function (response) {
+    if (response.status === 'success') {
+
+        var status = "<strong>" + this.arParams.messages.status + ":</strong> " + response.data.status;
+        $(this.arParams.status).html(status);
+
+        // Preparato set status bar and set progress to null
+        $(this.arParams.bar)
+            .removeClass("d-none")
+            .find(".progress-bar")
+            .animate({width:response.data.p + "%"}, 50)
+            .text(response.data.p + "%")
+            .addClass("progress-bar-animated")
+            .addClass("progress-bar-striped");
+
+        // Ajax
+        if(response.data.lastid > 0){
+            // send
+            BX.ajax.runComponentAction(
+                'studio7sbp:seller.personal.import.transfer',
+                'getElements',
+                {
+                    mode:'class',
+                    data: {
+                        work_start: "Y",
+                        lastid: response.data.lastid,
+                        sessid: BX.bitrix_sessid()
+                    }
+                }).then(BX.delegate(this.workOnload, this));
+        }else{
+            // stop
+            lancyImport.setStart(0);
+        }
+
+    }
 };
 
+lancyImport.prototype.showWaitWindow = function () {
+    var html = '<div class="product-add-loading">';
+        html += '<div class="main-ui-loader main-ui-loader-custom main-ui-show" data-is-shown="true" style="width: 130px; height: 130px;">';
+        html += '<svg class="main-ui-loader-svg" viewBox="25 25 50 50">';
+        html += '<circle class="main-ui-loader-svg-circle" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10" style="stroke: rgb(102, 58, 242);">';
+        html += '</circle>';
+        html += '</svg>';
+        html += '</div>';
+        html += '</div>';
+        $("body").append(html);
+};
 
+lancyImport.prototype.closeWaitWindow = function () {
+    $(".product-add-loading").remove();
+};
 
-
-
-/**
- * Using
- * @type {lancyImport}
- */
-var lancyImport = new lancyImport({
-    arParams: {param1: 1, param2: 2}
-});
-
-
-lancyImport.showStatus();
-
-lancyImport.setStart("set start");
-lancyImport.workOnload("result to eval");
