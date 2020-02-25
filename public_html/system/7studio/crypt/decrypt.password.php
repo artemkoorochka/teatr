a4e773933ad498520416d21ecdd3459f01b9352a
artemkoorochka <artem@koorochka.com>
2020-02-25 14:36:57 +0200

    isUserPassword

--- public_html/system/7studio/crypt/decrypt.password.php
+++ public_html/system/7studio/crypt/decrypt.password.php
@@ -1,35 +1,58 @@
 <?
-use Bitrix\Main\Authentication\ApplicationPasswordTable;
-
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->RestartBuffer();
 
-$users = CUser::GetList($by, $order, array("ID" => $USER->GetID()), array("FIELDS" => array("ID", "PASSWORD", "CHECKWORD", "DIGEST_PASSWORD", "APPLICATION_ID", "LOGIN", "EMAIL")));
+$user = CUser::GetList($by, $order, array("ID" => $USER->GetID()), array("FIELDS" => array("ID", "PASSWORD", "CHECKWORD", "DIGEST_PASSWORD", "APPLICATION_ID", "LOGIN", "EMAIL")));
 
-while ($user = $users->Fetch())
+if($user = $user->Fetch())
 {
-    d($user);
-
-    $PASSWORD = $user["PASSWORD"];
+    $PASSWORD = "";
     $CHECKWORD = $user["CHECKWORD"];
 
-
-
-
-
+    if(isUserPassword($user["ID"], $PASSWORD)){
+        d("it is user password");
+    }
+    else{
+        d("it is not user password");
+    }
 }
+
+d($user);
 
 
 /**
+ * The concepts:
  * PASSWORD
+ * CheckDBPassword
+ *
  * @param $external_user_id
  * @param $appPassword
  * @param bool $passwordOriginal
  */
-function decryptItbx($external_user_id, $appPassword, $passwordOriginal = true) {
+/**
+ * Проверяем, является ли $password текущим паролем пользователя.
+ *
+ * @param int $userId
+ * @param string $password
+ *
+ * @return bool
+ */
+function isUserPassword($userId, $password)
+{
+    $userData = CUser::GetByID($userId)->Fetch();
+
+    $salt = substr($userData['PASSWORD'], 0, (strlen($userData['PASSWORD']) - 32));
+
+    d($salt);
+
+    $realPassword = substr($userData['PASSWORD'], -32);
+
+    d($realPassword);
+
+    $password = md5($salt.$password);
 
 
+
+    return ($password == $realPassword);
 }
-
-
 
