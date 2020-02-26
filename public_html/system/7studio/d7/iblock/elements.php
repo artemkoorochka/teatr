6720ca9c06382ecd2d3ceec9bd3e447a131f55dd
artemkoorochka <artem@koorochka.com>
2020-02-26 16:38:35 +0200

    Use SQL Tracker

--- public_html/system/7studio/d7/iblock/elements.php
+++ public_html/system/7studio/d7/iblock/elements.php
@@ -3,12 +3,14 @@
  * https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=43&LESSON_ID=5753&LESSON_PATH=3913.5062.5748.5063.5753
  * https://dev.1c-bitrix.ru/support/forum/forum6/topic114267/
  * @var CMain $APPLICATION
- *
+ * Отладка SQL-запросов
+ * https://mrcappuccino.ru/blog/post/bitrix-d7-debug
  * TODO Нужно видить SQL
  */
 
 use Bitrix\Main\Loader,
-    Bitrix\Iblock\ElementTable;
+    Bitrix\Iblock\ElementTable,
+    Bitrix\Main\Diag;
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->RestartBuffer();
@@ -34,6 +36,11 @@ $arResult = array();
 
 Loader::includeModule($arParams["MODULE"]);
 
+/**
+ * Use Tracker
+ */
+$connection = Bitrix\Main\Application::getConnection();
+$tracker = $connection->startTracker(true); // Для того, чтобы очистить данные в трекере и начать новое отслеживание, нужно вызвать startTracker с аргументом $reset = true:
 $elements = ElementTable::getList(array(
     'order' => ["ID" => "asc"],
     "filter" => $arParams["ELEMENTS"]["FILTER"],
@@ -48,7 +55,13 @@ $elements = ElementTable::getList(array(
         ]
     ]
 ));
+$connection->stopTracker();
 
+foreach ($tracker->getQueries() as $query) {
+    d($query->getSql()); // Текст запроса
+    //d($query->getTrace()); // Стек вызовов функций, которые привели к выполнению запроса
+    d($query->getTime()); // Время выполнения запроса в секундах
+}
 
 while ($element = $elements->fetch()){
     $arResult[] = $element;
